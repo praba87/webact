805b1c33dcfbb42b51039e1801681e05a44049cd
prabha87 <prabhakaran.r@heymath.com>
2020-07-29 20:15:56 +0530

    webact2

--- webviewmodel/src/main/java/com/gameboxmini/webviewmodel/UrlUtils.java
+++ webviewmodel/src/main/java/com/gameboxmini/webviewmodel/UrlUtils.java
@@ -5,7 +5,7 @@ import java.net.URL;
 
 public class UrlUtils {
     public static String getHost(String url) throws MalformedURLException {
-        System.out.println("cc");
+        System.out.println("ccc");
         return new URL(url).getHost();
     }
 }
